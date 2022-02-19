956b1ebb1187a7bce4ce6d8e6490e3d904836590
baerrs <baerrs@gmail.com>
2022-02-19 00:59:04 -0500

    added a check of the ../ config files

--- config.py
+++ config.py
@@ -1,13 +1,28 @@
 # -- First you import Dynaconf
 from dynaconf import Dynaconf, Validator
+from os.path import exists
+from pathlib import Path
 
-# -- Then you create your `settings` instance
-settings = Dynaconf(
-    settings_files=["configs/settings.toml"],
-    environments=True,
-    load_dotenv=True,
-    envvar_prefix="DYNACONF",
-    env_switcher="ENV_FOR_DYNACONF",
-    dotenv_path="configs/.env"
-)
+if exists("../.Chia_WebSocket_MQTT") and Path("../.Chia_WebSocket_MQTT/settings.toml").is_file():
+    print("Loading settings from ../.Chia_WebSocket_MQTT/settings.toml & .env")
+    settings = Dynaconf(
+        # settings_files=["../.Chia_WebSocket_MQTT/settings.toml"],
+        environments=True,
+        load_dotenv=True,
+        envvar_prefix="DYNACONF",
+        env_switcher="ENV_FOR_DYNACONF",
+        dotenv_path="../.Chia_WebSocket_MQTT/.env",
+        settings_files=["../.Chia_WebSocket_MQTT/settings.toml"]
+    )
+else:
+    # -- Then you create your `settings` instance
+    print("using default settings")
+    settings = Dynaconf(
+        settings_files=["configs/settings.toml"],
+        environments=True,
+        load_dotenv=True,
+        envvar_prefix="DYNACONF",
+        env_switcher="ENV_FOR_DYNACONF",
+        dotenv_path="configs/.env"
+    )
 
